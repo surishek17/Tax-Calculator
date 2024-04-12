295a47d5f9b1c1cc030564b80fc3dad8cf2de47d
Shekhar <108945922+surishek17@users.noreply.github.com>
2024-04-12 22:26:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
-    $("#taxForm").submit(function(event) {
+$(document).ready(function () {
+    $("#taxForm").submit(function (event) {
         event.preventDefault();
         var income = parseFloat($("#income").val());
         var extraincome = parseFloat($("#extraincome").val());
         var age = $("#age").val();
         var deductions = parseFloat($("#deductions").val());
-        
+
         // Clear previous errors
         $(".error-icon").hide();
-        
+
         // Validate input fields
         var isValid = true;
         if (isNaN(income) || income <= 0) {
@@ -31,7 +31,7 @@ $(document).ready(function() {
             $("#ageErrorTooltip").show();
             isValid = false;
         }
-        
+
         if (isValid) {
             // Calculate tax
             var totalIncome = income + extraincome - deductions;
@@ -45,18 +45,18 @@ $(document).ready(function() {
                     tax = 0.1 * (totalIncome - 800000);
                 }
             }
-            
+
             // Display result in modal
             displayModal(tax);
         }
     });
-    
+
     // Close modal
-    $(".close").click(function() {
+    $(".close").click(function () {
         $("#modal").hide();
     });
 
-    $(window).click(function(event) {
+    $(window).click(function (event) {
         if (event.target == $("#modal")[0]) {
             $("#modal").hide();
         }
@@ -70,10 +70,52 @@ function displayModal(tax) {
 }
 
 // tooltip content hovering effect code
-$(document).ready(function() {
-    $('.question-mark').on('mouseover', function() {
+$(document).ready(function () {
+    $('.question-mark').on('mouseover', function () {
         $(this).next('.tooltip-content').show();
-    }).on('mouseout', function() {
+    }).on('mouseout', function () {
         $(this).next('.tooltip-content').hide();
+    });
+});
+
+
+$(document).ready(function () {
+    $('#income').on('input', function () {
+        var inputValue = $(this).val();
+        if (isNaN(inputValue)) {
+            $('#incomeErrorIcon').show();
+            $('#incomeErrorTooltip').text('Please enter numbers only');
+        } else {
+            $('#incomeErrorIcon').hide();
+            $('#incomeErrorTooltip').text('');
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $('#extraincome').on('input', function () {
+        var inputValue = $(this).val();
+        if (isNaN(inputValue)) {
+            $('#extraincomeErrorIcon').show();
+            $('#extraincomeErrorTooltip').text('Please enter numbers only');
+        } else {
+            $('#extraincomeErrorIcon').hide();
+            $('#extraincomeErrorTooltip').text('');
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $('#deductions').on('input', function () {
+        var inputValue = $(this).val();
+        if (isNaN(inputValue)) {
+            $('#deductionsErrorIcon').show();
+            $('#deductionsErrorTooltip').text('Please enter numbers only');
+        } else {
+            $('#deductionsErrorIcon').hide();
+            $('#deductionsErrorTooltip').text('');
+        }
     });
 });
